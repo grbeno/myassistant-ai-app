e4b4e85e6956fbae5291137dff7b3ad40b578369
grbeno <grbeno@gmail.com>
2023-03-15 01:12:12 +0100

    try-staticfiles

--- config/settings.py
+++ config/settings.py
@@ -158,9 +158,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ str(BASE_DIR.joinpath('build', 'static'))  ]
-
 STATIC_ROOT = str(BASE_DIR.joinpath('build', 'staticfiles'))
+
+STATICFILES_DIRS = [ str(BASE_DIR.joinpath('build', 'static'))  ]
 
 STATICFILES_FINDERS = [
   "django.contrib.staticfiles.finders.FileSystemFinder",
